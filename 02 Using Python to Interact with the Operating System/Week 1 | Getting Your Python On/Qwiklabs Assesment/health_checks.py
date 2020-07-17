001826848462339212a8827b1e1dc0b92ef3df1a
Elmo Allistair <work.elmoallistair@gmail.com>
2020-07-17 15:02:45 +0700

    Add Qwiklabs assesment

---
+++ 02 Using Python to Interact with the Operating System/Week 1 | Getting Your Python On/Qwiklabs Assesment/health_checks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import shutil
+import psutil
+from network import *
+
+def check_disk_usage(disk):
+    """Verifies that there's enough free space on disk"""
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    """Verifies that there's enough unused CPU"""
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+# If there's not enough disk, or not enough CPU, print an error
+if not check_disk_usage('/') or not check_cpu_usage():
+    print("ERROR!")
+elif check_localhost() and check_connectivity():
+    print("Everything ok")
---
+++ 02 Using Python to Interact with the Operating System/Week 1 | Getting Your Python On/Qwiklabs Assesment/network.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import requests
+import socket
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    return localhost == '127.0.0.1'
+
+def check_connectivity():
+    request = requests.get("http://www.google.com")
+    return 200
